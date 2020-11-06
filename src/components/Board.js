f54ba3c02dab0a3d64f66ea871dc17eb44da66c5
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-06 10:32:22 -0300

    Uploading finished game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import Square from './Square';
 
-const Board = props => <div>Board</div>;
+const style = {
+    border: '5px solid darkblue',
+    borderRadius: '10px',
+    width: '500px',
+    height: '500px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)',
+};
+
+const Board = ({ squares, onClick }) => (
+    <div style={style}>
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
+    </div>
+  );
+  
 
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Square = props => <div>Square</div>;
+const style = {
+    background: 'lightblue',
+    border: '5px solid darkblue',
+    fontSize: '50px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
+
+const Square = ({value, onClick}) => (
+    <button 
+        style={ style }
+        onClick={ onClick }
+    >
+        { value }
+    </button>
+);
 
 export default Square;
