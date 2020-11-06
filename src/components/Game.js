980b9d267bc12af21a8e6279948621f93b9efa94
Manuel Blanco <manuel.blanco77@gmail.com>
2020-11-06 09:23:29 -0300

    Setting up tic-tac-toe initial components and directory

---
+++ src/components/Game.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Game = props => <div>Game</div>;
+
+export default Game;
---
+++ src/components/Square.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Square = props => <div>Square</div>;
+
+export default Square;
